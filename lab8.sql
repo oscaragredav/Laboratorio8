77dfa47295ea1f5d54a6bc8e42adab28aafcbac2
Oscar <a20193315@pucp.edu.pe>
2023-06-15 21:44:42 -0500

    base de datos

---
+++ lab8.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: lab8
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `especialidad`
+--
+
+DROP TABLE IF EXISTS `especialidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `especialidad` (
+  `idEspecialidad` int NOT NULL,
+  `nombreEspecialidad` varchar(45) NOT NULL,
+  PRIMARY KEY (`idEspecialidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `especialidad`
+--
+
+LOCK TABLES `especialidad` WRITE;
+/*!40000 ALTER TABLE `especialidad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `especialidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estatus`
+--
+
+DROP TABLE IF EXISTS `estatus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estatus` (
+  `idestatus` int NOT NULL,
+  `nombreStatus` varchar(45) NOT NULL,
+  PRIMARY KEY (`idestatus`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estatus`
+--
+
+LOCK TABLES `estatus` WRITE;
+/*!40000 ALTER TABLE `estatus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estatus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seguro`
+--
+
+DROP TABLE IF EXISTS `seguro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seguro` (
+  `idseguro` int NOT NULL,
+  `nombreSeguro` varchar(45) NOT NULL,
+  PRIMARY KEY (`idseguro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seguro`
+--
+
+LOCK TABLES `seguro` WRITE;
+/*!40000 ALTER TABLE `seguro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seguro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `edad` int NOT NULL,
+  `codigo` int NOT NULL,
+  `idEspecialidad` int NOT NULL,
+  `estatus` int NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `passwordHashed` varchar(45) NOT NULL,
+  `estatus_idestatus` int NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  KEY `fk_usuario_especialidad_idx` (`idEspecialidad`),
+  KEY `fk_usuario_estatus1_idx` (`estatus_idestatus`),
+  CONSTRAINT `fk_usuario_especialidad` FOREIGN KEY (`idEspecialidad`) REFERENCES `especialidad` (`idEspecialidad`),
+  CONSTRAINT `fk_usuario_estatus1` FOREIGN KEY (`estatus_idestatus`) REFERENCES `estatus` (`idestatus`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `viaje`
+--
+
+DROP TABLE IF EXISTS `viaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `viaje` (
+  `idviaje` int NOT NULL AUTO_INCREMENT,
+  `fechaReserva` date NOT NULL,
+  `fechaViaje` varchar(45) NOT NULL,
+  `ciudadOrigen` varchar(45) NOT NULL,
+  `ciudadDestino` varchar(45) NOT NULL,
+  `cantidad` int NOT NULL,
+  `costoUnitario` int NOT NULL,
+  `costoTotal` int NOT NULL,
+  `idseguro` int NOT NULL,
+  `usuario_idUsuario` int NOT NULL,
+  PRIMARY KEY (`idviaje`),
+  KEY `fk_viaje_seguro1_idx` (`idseguro`),
+  KEY `fk_viaje_usuario1_idx` (`usuario_idUsuario`),
+  CONSTRAINT `fk_viaje_seguro1` FOREIGN KEY (`idseguro`) REFERENCES `seguro` (`idseguro`),
+  CONSTRAINT `fk_viaje_usuario1` FOREIGN KEY (`usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `viaje`
+--
+
+LOCK TABLES `viaje` WRITE;
+/*!40000 ALTER TABLE `viaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `viaje` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-15 21:39:11
